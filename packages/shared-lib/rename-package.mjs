641182b9ec437443b22acbc90e312838aab8cd85
Tom Lee <t.lee@evs.com>
2022-09-23 17:02:42 +0100

    chore: Add `sofie-` to published shared-apis package name

--- packages/shared-lib/rename-package.mjs
+++ packages/shared-lib/rename-package.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
 const packageFile = JSON.parse(fs.readFileSync('package.json'))
-packageFile.name = `@evs/${packageFile.name.replace('@sofie-automation/', '')}`
+packageFile.name = `@evs/sofie-${packageFile.name.replace('@sofie-automation/', '')}`
 
 fs.writeFileSync('package.json', JSON.stringify(packageFile, null, 4))
