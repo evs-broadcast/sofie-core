cb4f99c43958deaaf0fea6373a2a13f191a74a32
Tom Lee <t.lee@evs.com>
2023-06-02 18:02:38 +0100

    test: Trailing slash for npm registry

--- set_npm_credentials.sh
+++ set_npm_credentials.sh
@@ -1,6 +1,6 @@
 rm -f $HOME/.npmrc
 echo "email=${NPM_USER}@evs.com" >> $HOME/.npmrc
-echo "@evs:registry=${NPM_REGISTRY_SNAPSHOT}" >> $HOME/.npmrc
+echo "@evs:registry=${NPM_REGISTRY_SNAPSHOT}/" >> $HOME/.npmrc
 echo "//${NPM_REGISTRY_SNAPSHOT}/:_auth=${NPM_AUTH_TOKEN}" >> $HOME/.npmrc
 echo "//${NPM_REGISTRY_SNAPSHOT}/:email=${NPM_USER}@evs.com" >> $HOME/.npmrc
 echo "//${NPM_REGISTRY_SNAPSHOT}/:always-auth=true" >> $HOME/.npmrc
