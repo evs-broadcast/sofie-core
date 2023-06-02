71a89dd836a54aede64e4a74367b8ecc9d7d3897
Tom Lee <t.lee@evs.com>
2023-06-02 16:01:35 +0100

    fix: Remove reliance on npm config fix

--- set_npm_credentials.sh
+++ set_npm_credentials.sh
@@ -1,11 +1,9 @@
 rm -f $HOME/.npmrc
-echo "_auth=${NPM_AUTH_TOKEN}" >> $HOME/.npmrc
 echo "email=${NPM_USER}@evs.com" >> $HOME/.npmrc
 echo "always-auth=true" >> $HOME/.npmrc
 echo "@evs:registry=${NPM_REGISTRY_SNAPSHOT}" >> $HOME/.npmrc
+echo "//${NPM_REGISTRY_SNAPSHOT}/:_auth=${NPM_AUTH_TOKEN}" >> $HOME/.npmrc
 echo "//${NPM_REGISTRY_SNAPSHOT}/:_password=${NPM_ENCODED_PASSWORD}" >> $HOME/.npmrc
 echo "//${NPM_REGISTRY_SNAPSHOT}/:username=${NPM_USER}" >> $HOME/.npmrc
 echo "//${NPM_REGISTRY_SNAPSHOT}/:email=${NPM_USER}@evs.com" >> $HOME/.npmrc
 echo "//${NPM_REGISTRY_SNAPSHOT}/:always-auth=true" >> $HOME/.npmrc
-
-# npm config fix
