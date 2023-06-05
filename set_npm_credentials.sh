e751326dfdcf8f8da35bd4192f55cc1d5785abd5
Tom Lee <t.lee@evs.com>
2023-06-05 13:09:31 +0100

    feat: npm adduser magic

--- set_npm_credentials.sh
+++ set_npm_credentials.sh
@@ -1,6 +1,9 @@
 rm -f $HOME/.npmrc
-echo "email=${NPM_USER}@evs.com" >> $HOME/.npmrc
-echo "@evs:registry=${NPM_REGISTRY_SNAPSHOT}/" >> $HOME/.npmrc
-echo "//${NPM_REGISTRY_SNAPSHOT}/:_auth=${NPM_AUTH_TOKEN}" >> $HOME/.npmrc
-echo "//${NPM_REGISTRY_SNAPSHOT}/:email=${NPM_USER}@evs.com" >> $HOME/.npmrc
-echo "//${NPM_REGISTRY_SNAPSHOT}/:always-auth=true" >> $HOME/.npmrc
+/usr/bin/expect <<EOD
+spawn npm adduser --registry "$NPM_REGISTRY_SNAPSHOT/" --scope @evs --auth-type legacy
+expect {
+  "Username:" {send "$NPM_USER\r"; exp_continue}
+  "Password:" {send "$NPM_PASSWORD\r"; exp_continue}
+  "Email: (this IS public)" {send "$NPM_EMAIL\r"; exp_continue}
+}
+EOD
